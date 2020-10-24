19aa1bc1adbfa2e38dd5c6526345f93815ddbcb0
Yulin Karaiscos <ykaraiscos@yahoo.com>
2020-10-24 11:37:11 -0700

    Updates

---
+++ precache-manifest.6407c5b7778ccf46a0879300151b7c0d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f70c91be37c8e242543ed3b0449c8b",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "d05c752bcf792d41aae0",
+    "url": "/react_portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a4db983271d0691426be",
+    "url": "/react_portfolio/static/css/main.fbd8d1a2.chunk.css"
+  },
+  {
+    "revision": "d05c752bcf792d41aae0",
+    "url": "/react_portfolio/static/js/2.7638979a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react_portfolio/static/js/2.7638979a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4db983271d0691426be",
+    "url": "/react_portfolio/static/js/main.7a9db92d.chunk.js"
+  },
+  {
+    "revision": "b56ced57923cdb2512c8",
+    "url": "/react_portfolio/static/js/runtime-main.c2d1ad6e.js"
+  },
+  {
+    "revision": "c7e2713c4a784bda3bc7dea164bacaa9",
+    "url": "/react_portfolio/static/media/alcoCallV3.c7e2713c.png"
+  },
+  {
+    "revision": "e2caa0dcde8922b248ab258a01402900",
+    "url": "/react_portfolio/static/media/dayPlanner.e2caa0dc.png"
+  },
+  {
+    "revision": "39f37be7a9e5e8f7e276441acc2c35b5",
+    "url": "/react_portfolio/static/media/eat-da-burger.39f37be7.png"
+  },
+  {
+    "revision": "c0c96dcf61862622b702572f0160496b",
+    "url": "/react_portfolio/static/media/weatherApp.c0c96dcf.png"
+  }
+]);
